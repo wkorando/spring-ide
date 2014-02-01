8a672c8215caa1f0b068e91085a7888ac4ab9075
Miles Parker <milesparker@gmail.com>
2014-01-31 18:56:41 -0800

    Tree editor support.

--- plugins/org.springframework.ide.eclipse.beans.ui.livegraph/src/org/springframework/ide/eclipse/beans/ui/livegraph/views/LiveBeansGraphView.java
+++ plugins/org.springframework.ide.eclipse.beans.ui.livegraph/src/org/springframework/ide/eclipse/beans/ui/livegraph/views/LiveBeansGraphView.java
@@ -10,8 +10,20 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.beans.ui.livegraph.views;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
@@ -19,23 +31,12 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.viewers.DoubleClickEvent;
-import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.ViewerSorter;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.actions.BaseSelectionListenerAction;
-import org.eclipse.ui.part.PageBook;
-import org.eclipse.ui.part.ViewPart;
-import org.eclipse.zest.core.viewers.GraphViewer;
-import org.eclipse.zest.core.widgets.ZestStyles;
-import org.eclipse.zest.layouts.LayoutStyles;
 import org.springframework.ide.eclipse.beans.ui.livegraph.LiveGraphUiPlugin;
 import org.springframework.ide.eclipse.beans.ui.livegraph.actions.ConnectToApplicationAction;
 import org.springframework.ide.eclipse.beans.ui.livegraph.actions.FilterInnerBeansAction;
@@ -45,15 +46,29 @@ import org.springframework.ide.eclipse.beans.ui.livegraph.actions.OpenBeanDefini
 import org.springframework.ide.eclipse.beans.ui.livegraph.actions.RefreshApplicationAction;
 import org.springframework.ide.eclipse.beans.ui.livegraph.actions.ToggleGroupByAction;
 import org.springframework.ide.eclipse.beans.ui.livegraph.actions.ToggleViewModeAction;
+import org.springframework.ide.eclipse.beans.ui.livegraph.model.LiveBean;
+import org.springframework.ide.eclipse.beans.ui.livegraph.model.LiveBeansGroup;
 import org.springframework.ide.eclipse.beans.ui.livegraph.model.LiveBeansModel;
 import org.springframework.ide.eclipse.beans.ui.livegraph.model.LiveBeansModelCollection;
+import org.springsource.ide.eclipse.commons.browser.javafx.JavaFxBrowserView;
+import org.springsource.ide.eclipse.commons.gettingstarted.dashboard.DashboardCopier;
 
 /**
  * A simple view to host our graph
  * 
  * @author Leo Dos Santos
  */
-public class LiveBeansGraphView extends ViewPart {
+public class LiveBeansGraphView extends JavaFxBrowserView {
+
+	private static final String HTML_PAGE_GRAPH = "liveBeansGraph.html";
+
+	private static final String HTML_PAGE_TREE = "liveBeansTree.html";
+
+	private static final String LIVE_BEANS_ROOT_URI = "platform:/plugin/org.springframework.ide.eclipse.beans.ui.livegraph/resources/livebeans";
+
+	private static final String LINE_FEED = "\n";
+
+	private File htmlRoot;
 
 	public static final String VIEW_ID = "org.springframework.ide.eclipse.beans.ui.livegraph.views.LiveBeansGraphView";
 
@@ -77,8 +92,6 @@ public class LiveBeansGraphView extends ViewPart {
 
 	private ToggleGroupByAction[] groupByActions;
 
-	private final MultiViewerSelectionProvider selectionProvider;
-
 	private BaseSelectionListenerAction openBeanClassAction;
 
 	private BaseSelectionListenerAction openBeanDefAction;
@@ -91,82 +104,48 @@ public class LiveBeansGraphView extends ViewPart {
 
 	private Action connectApplicationAction;
 
-	private PageBook pagebook;
-
-	private GraphViewer graphViewer;
-
-	private TreeViewer treeViewer;
-
 	private final IPreferenceStore prefStore;
 
 	private int activeDisplayMode;
 
 	private int activeGroupByMode;
 
-	public LiveBeansGraphView() {
-		super();
+	private ITreeContentProvider treeContentProvider;
+
+	public LiveBeansGraphView() throws URISyntaxException, IOException {
+		super("", false);
 		prefStore = LiveGraphUiPlugin.getDefault().getPreferenceStore();
-		selectionProvider = new MultiViewerSelectionProvider();
 		innerBeansFilter = new InnerBeansViewerFilter();
 	}
 
-	private void createGraphViewer() {
-		graphViewer = new GraphViewer(pagebook, SWT.NONE);
-		graphViewer.setContentProvider(new LiveBeansGraphContentProvider());
-		graphViewer.setLabelProvider(new LiveBeansGraphLabelProvider());
-		graphViewer.setConnectionStyle(ZestStyles.CONNECTIONS_DIRECTED);
-		// viewer.setNodeStyle(ZestStyles.NODES_FISHEYE);
-
-		ExtendedDirectedGraphLayoutAlgorithm layout = new ExtendedDirectedGraphLayoutAlgorithm(
-				LayoutStyles.NO_LAYOUT_NODE_RESIZING | SWT.HORIZONTAL);
-		graphViewer.setLayoutAlgorithm(layout);
-
-		graphViewer.addDoubleClickListener(new IDoubleClickListener() {
-			public void doubleClick(DoubleClickEvent event) {
-				if (openBeanClassAction != null && openBeanClassAction.isEnabled()) {
-					openBeanClassAction.run();
-				}
-			}
-		});
+	private void setPage(String pageName) {
+		try {
+			URL fileURL = FileLocator.toFileURL(new URL(LIVE_BEANS_ROOT_URI));
+			htmlRoot = DashboardCopier.getCopy(new File(fileURL.toURI()), new NullProgressMonitor());
+			File liveBeansUrl = new File(htmlRoot, pageName);
+			setUrl(liveBeansUrl.toURI().toString());
+		}
+		catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		pagebook = new PageBook(parent, SWT.NONE);
-		getViewSite().setSelectionProvider(selectionProvider);
+		super.createPartControl(parent);
 		makeActions();
-		createGraphViewer();
-		createTreeViewer();
 		hookToolBar();
 		hookPullDownMenu();
 		hookContextMenu();
-		selectionProvider.addSelectionChangedListener(openBeanClassAction);
-		selectionProvider.addSelectionChangedListener(openBeanDefAction);
 		setDisplayMode(prefStore.getInt(PREF_DISPLAY_MODE));
 		setGroupByMode(prefStore.getInt(PREF_GROUP_MODE));
 		setFilterInnerBeans(prefStore.getBoolean(PREF_FILTER_INNER_BEANS));
-	}
-
-	private void createTreeViewer() {
-		treeViewer = new TreeViewer(pagebook, SWT.NONE);
-		treeViewer.setContentProvider(new LiveBeansTreeContentProvider(this));
-		treeViewer.setLabelProvider(new LiveBeansTreeLabelProvider());
-		treeViewer.setSorter(new ViewerSorter());
-
-		treeViewer.addDoubleClickListener(new IDoubleClickListener() {
-			public void doubleClick(DoubleClickEvent event) {
-				if (openBeanDefAction != null && openBeanDefAction.isEnabled()) {
-					openBeanDefAction.run();
-				}
-			}
-		});
-	}
-
-	@Override
-	public void dispose() {
-		selectionProvider.removeSelectionChangedListener(openBeanClassAction);
-		selectionProvider.removeSelectionChangedListener(openBeanDefAction);
-		super.dispose();
 	}
 
 	private void fillContextMenu(IMenuManager menuManager) {
@@ -217,10 +196,6 @@ public class LiveBeansGraphView extends ViewPart {
 				}
 			}
 		});
-
-		graphViewer.getControl().setMenu(menuManager.createContextMenu(graphViewer.getControl()));
-		treeViewer.getControl().setMenu(menuManager.createContextMenu(treeViewer.getControl()));
-		getViewSite().registerContextMenu(menuManager, selectionProvider);
 	}
 
 	private void hookPullDownMenu() {
@@ -263,10 +238,10 @@ public class LiveBeansGraphView extends ViewPart {
 
 	public void setDisplayMode(int mode) {
 		if (mode == DISPLAY_MODE_GRAPH) {
-			pagebook.showPage(graphViewer.getControl());
+			setPage(HTML_PAGE_GRAPH);
 		}
 		else if (mode == DISPLAY_MODE_TREE) {
-			pagebook.showPage(treeViewer.getControl());
+			setPage(HTML_PAGE_TREE);
 		}
 		for (ToggleViewModeAction action : displayModeActions) {
 			action.setChecked(mode == action.getDisplayMode());
@@ -277,96 +252,152 @@ public class LiveBeansGraphView extends ViewPart {
 
 	@Override
 	public void setFocus() {
-		if (isViewerVisible(graphViewer)) {
-			graphViewer.getControl().setFocus();
-		}
-		else if (isViewerVisible(treeViewer)) {
-			treeViewer.getControl().setFocus();
-		}
+		getBrowserViewer().setFocus();
 	}
 
 	public void setGroupByMode(int mode) {
 		activeGroupByMode = mode;
-		if (isViewerVisible(treeViewer)) {
-			treeViewer.refresh();
-		}
-		for (ToggleGroupByAction action : groupByActions) {
-			action.setChecked(mode == action.getGroupByMode());
-		}
+		// if (isViewerVisible(treeViewer)) {
+		// treeViewer.refresh();
+		// }
+		// for (ToggleGroupByAction action : groupByActions) {
+		// action.setChecked(mode == action.getGroupByMode());
+		// }
 		prefStore.setValue(PREF_GROUP_MODE, mode);
+	}
+
+	private static String jsonValue(String name, String value) {
+		return "\"" + name + "\":\"" + value + "\"";
+	}
+
+	private static String jsonValue(String name, Integer value) {
+		return "\"" + name + "\":" + value;
+	}
+
+	private static String jsonValue(String name, Boolean value) {
+		return "\"" + name + "\":" + value;
+	}
+
+	private static String jsonObject(String values) {
+		return "{" + values + "}";
+	}
+
+	private static String jsonArray(String name, List<String> values) {
+		return "\"" + name + "\": [" + LINE_FEED + StringUtils.join(values, "," + LINE_FEED) + "]" + LINE_FEED;
 	}
 
 	public void setInput(LiveBeansModel model) {
 		activeInput = model;
-		if (graphViewer != null) {
-			graphViewer.setInput(activeInput);
+
+		List<String> nodes = new ArrayList<String>();
+		List<String> links = new ArrayList<String>();
+		List<LiveBean> beans = model.getBeans();
+		int nodeIndex = 0;
+		for (LiveBean bean : beans) {
+			nodes.add(jsonObject(jsonBeanValues(nodeIndex, bean)));
+			for (LiveBean dependent : bean.getDependencies()) {
+				int dependentIndex = beans.indexOf(dependent);
+				if (dependentIndex >= 0) {
+					String link = jsonObject(jsonValue("source", nodeIndex) + ", "
+							+ jsonValue("target", dependentIndex) + ", " + jsonValue("value", 1) + ", "
+							+ jsonValue("left", false) + ", " + jsonValue("right", true));
+					links.add(link);
+				}
+			}
+			nodeIndex++;
 		}
-		if (treeViewer != null) {
-			treeViewer.setInput(activeInput);
+		String jsonData = jsonObject(jsonArray("nodes", nodes) + "," + LINE_FEED + jsonArray("links", links));
+		writeDataFile("graphData.json", jsonData);
+
+		treeContentProvider = new LiveBeansTreeContentProvider(this);
+		List<String> rootChildren = new ArrayList<String>();
+		Object[] elements = treeContentProvider.getElements(model);
+		for (Object object : elements) {
+			LiveBeansGroup bean = (LiveBeansGroup) object;
+			rootChildren.add(jsonSubTree(bean));
+		}
+		String treeJson = jsonObject(jsonValue("name", "Root") + "," + jsonArray("children", rootChildren));
+		writeDataFile("treeData.json", treeJson);
+	}
+
+	private String jsonSubTree(LiveBeansGroup group) {
+		Object[] children = treeContentProvider.getChildren(group);
+		List<String> levelChildren = new ArrayList<String>();
+		for (Object object : children) {
+			LiveBean childBean = (LiveBean) object;
+			levelChildren.add(jsonSubTree(childBean, new HashSet<LiveBean>()));
+		}
+		return jsonObject(jsonValue("name", group.getDisplayName()) + "," + jsonArray("children", levelChildren));
+	}
+
+	private String jsonSubTree(LiveBean bean, Set<LiveBean> parents) {
+		Set<LiveBean> children = new HashSet<LiveBean>();
+		Set<LiveBean> dependencies = bean.getDependencies();
+		for (LiveBean child : dependencies) {
+			children.add(child);
+		}
+		// Set<LiveBean> injectInto = bean.getInjectedInto();
+		// for (LiveBean child : injectInto) {
+		// children.add(child);
+		// }
+
+		List<String> levelChildren = new ArrayList<String>();
+		for (LiveBean childBean : children) {
+			if (parents.contains(childBean)) {
+				continue;
+			}
+			System.err.println(childBean.getDisplayName());
+			Set<LiveBean> parentsPlus = new HashSet<LiveBean>(parents);
+			parentsPlus.add(childBean);
+			levelChildren.add(jsonSubTree(childBean, parentsPlus));
+		}
+		return jsonObject(jsonBeanValues(1, bean) + "," + jsonArray("children", levelChildren));
+	}
+
+	private String jsonBeanValues(int nodeIndex, LiveBean bean) {
+		return jsonValue("id", nodeIndex) + ", "// + jsonValue("beanId",
+												// bean.getId()) + ", "
+				// + jsonValue("beanType", bean.getBeanType()) + ", "
+				// + jsonValue("applicationName", bean.getApplicationName()) +
+				// ", "
+				+ jsonValue("name", bean.getDisplayName());
+		// + ", " + jsonValue("resource", bean.getResource()) + ", "
+		// + jsonValue("group", 1);
+	}
+
+	private void writeDataFile(String fileName, String data) {
+		System.err.println(data);
+		File json = new File(htmlRoot, fileName);
+		try {
+			json.createNewFile();
+			FileWriter fileWriter = new FileWriter(json);
+			fileWriter.write(data);
+			fileWriter.close();
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
 	public void setFilterInnerBeans(boolean filtered) {
-		if (graphViewer != null) {
-			if (filtered) {
-				graphViewer.addFilter(innerBeansFilter);
-			}
-			else {
-				graphViewer.removeFilter(innerBeansFilter);
-			}
-			graphViewer.applyLayout();
-		}
-		if (treeViewer != null) {
-			if (filtered) {
-				treeViewer.addFilter(innerBeansFilter);
-			}
-			else {
-				treeViewer.removeFilter(innerBeansFilter);
-			}
-		}
+		// if (graphViewer != null) {
+		// if (filtered) {
+		// graphViewer.addFilter(innerBeansFilter);
+		// }
+		// else {
+		// graphViewer.removeFilter(innerBeansFilter);
+		// }
+		// graphViewer.applyLayout();
+		// }
+		// if (treeViewer != null) {
+		// if (filtered) {
+		// treeViewer.addFilter(innerBeansFilter);
+		// }
+		// else {
+		// treeViewer.removeFilter(innerBeansFilter);
+		// }
+		// }
 		filterInnerBeansAction.setChecked(filtered);
 		prefStore.setValue(PREF_FILTER_INNER_BEANS, filtered);
 	}
-
-	private class MultiViewerSelectionProvider implements ISelectionProvider {
-
-		public void addSelectionChangedListener(ISelectionChangedListener listener) {
-			if (graphViewer != null) {
-				graphViewer.addSelectionChangedListener(listener);
-			}
-			if (treeViewer != null) {
-				treeViewer.addSelectionChangedListener(listener);
-			}
-		}
-
-		public ISelection getSelection() {
-			if (isViewerVisible(graphViewer)) {
-				return graphViewer.getSelection();
-			}
-			if (isViewerVisible(treeViewer)) {
-				return treeViewer.getSelection();
-			}
-			return null;
-		}
-
-		public void removeSelectionChangedListener(ISelectionChangedListener listener) {
-			if (graphViewer != null) {
-				graphViewer.removeSelectionChangedListener(listener);
-			}
-			if (treeViewer != null) {
-				treeViewer.removeSelectionChangedListener(listener);
-			}
-		}
-
-		public void setSelection(ISelection selection) {
-			if (isViewerVisible(graphViewer)) {
-				graphViewer.setSelection(selection);
-			}
-			else if (isViewerVisible(treeViewer)) {
-				treeViewer.setSelection(selection);
-			}
-		}
-
-	}
-
 }
